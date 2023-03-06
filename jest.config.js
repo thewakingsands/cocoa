c74ea7b6ffdc7f473bbbeb572d00ceb63d5ee576
Innpaul Zhang <zyp108421@gmail.com>
2023-03-07 01:20:30 +0800

    chore: setup jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/test/**'],
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  verbose: true,
 }
+
+module.exports = config
